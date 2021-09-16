b80f17eed9b89239f2cca3dd145749820f4985c5
dianacato <diana.catolico@hotmail.com>
2021-09-16 09:22:12 -0500

    bug

--- Laboratoria-WhatsApp-plugin/admin/data-base.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if(isset($_POST["enterpriseName"])){
-        $enterpriseName = $_POST["enterpriseName"];
-        $enterpriseNumber = $_POST["number"];
-        $enterpriseMessage = $_POST["message"];
-        var_dump($_POST["enterpriseName"]);
-    }
-?>
