a66ad101dd7db044b8414a1031da34ad815ca630
dianacato <diana.catolico@hotmail.com>
2021-09-16 09:31:01 -0500

    solved bug

---
+++ Laboratoria-WhatsApp-plugin/admin/data-base.php
@@ -0,0 +1,8 @@
+<?php
+    if(isset($_POST["enterpriseName"])){
+        $enterpriseName = $_POST["enterpriseName"];
+        $enterpriseNumber = $_POST["number"];
+        $enterpriseMessage = $_POST["message"];
+        var_dump($_POST["enterpriseName"]);
+    }
+?>
---
+++ Laboratoria-WhatsApp-plugin/admin/whatsapp-plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+echo    '<div id="svelte-admin"></div>';
+
+require_once(plugin_dir_path(__FILE__).'data-base.php');
+
+// if(isset($_POST["enterpriseName"])){
+//     $enterpriseName = $_POST["enterpriseName"];
+//     $enterpriseNumber = $_POST["number"];
+//     $enterpriseMessage = $_POST["message"];
+//     var_dump($_POST["enterpriseName"]);
+// }
