32030cd6a7bdaa1a8b741a9e1931b00b55abb780
Natalia Garavito <garavitobarretonatalia@gmail.com>
2021-09-21 09:30:44 -0500

    insert into database files from form
    confirmar espacios de form no esten vacios, despues insertar los valores obtenidos en el form a la base de datos (data-base-php)

--- html/wp-content/plugins/Laboratoria-WhatsApp-plugin/admin/data-base.php
+++ html/wp-content/plugins/Laboratoria-WhatsApp-plugin/admin/data-base.php
@@ -1,8 +1,20 @@
-<?php
-    if(isset($_POST["enterpriseName"])){
+<?php        
+    global $wpdb;
+
+    if((empty($_POST["enterpriseName"])) || (empty($_POST["number"])) || (empty($_POST["message"]))){
+        echo "missing spaces";  
+    } else
+    {
         $enterpriseName = $_POST["enterpriseName"];
-        $enterpriseNumber = $_POST["number"];
+        $enterpriseNumber = $_POST["select"].$_POST["number"];
         $enterpriseMessage = $_POST["message"];
-        var_dump($_POST["select"]);
+        $sql = "INSERT INTO wp_WhatsAppPlugin ". "(enterprise_name, number, message) ". "VALUES ('$enterpriseName', '$enterpriseNumber', '$enterpriseMessage')";      
+
+        if ($wpdb->query($sql)) {
+            echo "New record created successfully";
+          } else {
+            echo "Error: " . $sql . "<br>" . $wpdb->error;
+          }
     }
+    
 ?>
